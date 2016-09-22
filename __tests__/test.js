46a311dd28f353a63f29d73e8c2e585139112beb
Henrik Hansson <henrik.hansson@hitta.se>
2016-09-22 13:37:53 +0200

    Basic functionality to return config based on environment name.

--- __tests__/test.js
+++ __tests__/test.js
@@ -11,3 +11,48 @@ it("should return the loaded json if no special keys are present", () => {
     expect(config.a).toBe(1);
     expect(config.b).toBe("2");
 });
+
+it("should return different strings based on env", () => {
+    const defaultConfig = generateConfig({
+        environment: "custom",
+        file: "environmentDependent.json",
+        dir: path.join(__dirname, "testConfigs")
+    });
+
+    const devConfig = generateConfig({
+        environment: "dev",
+        file: "environmentDependent.json",
+        dir: path.join(__dirname, "testConfigs")
+    });
+
+    expect(defaultConfig).toBeDefined();
+    expect(defaultConfig.env).toBe("default");
+    expect(defaultConfig.a).toBe(1);
+
+    expect(devConfig).toBeDefined();
+    expect(devConfig.env).toBe("development");
+    expect(devConfig.a).toBe(1);
+});
+
+it("should return different objects based on env", () => {
+    const devConfig = generateConfig({
+        environment: "dev",
+        file: "environmentDependent.json",
+        dir: path.join(__dirname, "testConfigs")
+    });
+
+    const prodConfig = generateConfig({
+        environment: "prod",
+        file: "environmentDependent.json",
+        dir: path.join(__dirname, "testConfigs")
+    });
+
+    expect(devConfig.keys.key1).toBeDefined();
+    expect(devConfig.keys.key2).not.toBeDefined();
+    expect(devConfig.keys.key1).toBe("val def");
+
+    expect(prodConfig.keys.key1).toBeDefined();
+    expect(prodConfig.keys.key2).toBeDefined();
+    expect(prodConfig.keys.key1).toBe("val prod");
+    expect(prodConfig.keys.key2).toBe("only prod");
+});
--- index.js
+++ index.js
@@ -6,14 +6,38 @@ function loadFile(filePath) {
   return fileInfo.isFile() ? require(filePath) : undefined;
 }
 
+function isObject(obj) {
+  return obj && typeof obj === "object" && !Array.isArray(obj);
+}
+
+function handleObj(obj, currentEnv, defaultEnv) {
+  if (!isObject(obj)) {
+    return obj;
+  }
+  if (obj[currentEnv]) {
+    return obj[currentEnv];
+  }
+  if (obj[defaultEnv]) {
+    return obj[defaultEnv];
+  }
+  const retObj = {};
+  Object.keys(obj).forEach((key) => {
+    retObj[key] = handleObj(obj[key], currentEnv, defaultEnv);
+  });
+  return retObj;
+}
+
 function generateConfig(options = {}) {
   const envName = options.environment || process.env.NODE_ENV || "dev";
   const inputFile = options.file || "config.json";
   const basePath = options.dir || process.env.CONFIG_BASE_PATH || process.cwd();
+  const defaultEnvironment = "default";
 
   const filePath = path.join(basePath, inputFile);
 
-  return loadFile(filePath);
+  const inputConfig = loadFile(filePath);
+
+  return handleObj(inputConfig, envName, defaultEnvironment);
 }
 
 module.exports = generateConfig;
